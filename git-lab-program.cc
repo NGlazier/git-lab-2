955626579865a64f88fece0ab15c59bec75cd3a3
Noah Glazier <ng190817@ohio.edu>
2019-01-22 19:01:00 -0500

    ng190817@ohio.edu

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Noah Glazier
+ *        Date: January 22, 2019
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!";
+    return 0;
+}// main
